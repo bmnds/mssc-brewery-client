4643bf979489fe7a4457aa8dfbc304ddb9e7e331
Bruno <Bruno@ATLANTICA>
2019-12-14 16:22:38 -0400

    Testing Save Beer and Customer Endpoints

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbreweryclient.web.client;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.Customer;
 import lombok.Setter;
 
 @Component
@@ -15,6 +17,7 @@ import lombok.Setter;
 public class BreweryClient {
 	
 	private final String BEER_PATH_V1 = "/api/v1/beer/";
+	private final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
 	
 	@Setter
 	private String apiHost;
@@ -27,6 +30,18 @@ public class BreweryClient {
 	
 	public BeerDto getBeerById(UUID uuid) {
 		return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid, BeerDto.class);
+	}
+	
+	public URI saveBeer(BeerDto beer) {
+		return restTemplate.postForLocation(apiHost + BEER_PATH_V1, beer);
+	}
+
+	public Customer getCustomerById(UUID uuid) {
+		return restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + uuid, Customer.class);
+	}
+	
+	public URI saveCustomer(Customer customer) {
+		return restTemplate.postForLocation(apiHost + CUSTOMER_PATH_V1, customer);
 	}
 
 }
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbreweryclient.web.client;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -9,16 +10,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.Customer;
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
+@Log4j2
 class BreweryClientTest {
-	
-	@Autowired BreweryClient client;
+
+	@Autowired
+	BreweryClient client;
 
 	@Test
-	void test() {
-		 BeerDto dto = client.getBeerById(UUID.randomUUID());
-		 assertNotNull(dto);
+	void testGetBeer() {
+		BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+		assertNotNull(dto);
+	}
+	
+	@Test
+	void testSaveBeer() {
+		BeerDto beer = BeerDto.builder().beerName("Beer Creation Test").build();
+		
+		URI uri = client.saveBeer(beer);
+		
+		assertNotNull(uri);
+	}
+	
+	@Test
+	void testGetCustomer() {
+		UUID uuid = UUID.randomUUID();
+		
+		log.debug("Trying to get customer identified by {}", uuid);
+		Customer customer = client.getCustomerById(uuid);
+		
+		assertNotNull(customer);
+		log.debug("Successfully got customer {}", customer);
+	}
+	
+	@Test
+	void testSaveCustomer() {
+		Customer customer = Customer.builder().name("Customer Creation Test").build();
+		
+		log.debug("Trying to save customer {}", customer);
+		URI uri = client.saveCustomer(customer);
+		
+		assertNotNull(uri);
+		log.debug("Successfully saved customer at {}", uri);
 	}
 
 }
